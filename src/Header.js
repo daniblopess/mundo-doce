6725806407a4d4c478e5ea4bc065858670bbdd82
Daniela Lopes <danielablopess@gmail.com>
2020-12-04 23:57:17 -0300

    Primeiro commit

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+function Header(){
+    return (
+        <header className="App-header">
+            <div className="Top-header">
+                Mundo Doce      
+            </div>
+            <div className="Sub-header">
+                Sua loja favorita para adoçar seu mundo!
+            </div>
+    </header>
+    )
+}
+
+export default Header;
---
+++ src/MainPage.js
@@ -0,0 +1,73 @@
+import amora from './assets/amora.png';
+
+function MainPage(){
+    return (
+        <main className="main">            
+            <div className="card">
+                <img src={amora} class="card-img-top text-center" alt="Balas de Amora Fini" />
+                <div className="card-body">
+                    <h6 className="card-title">Balas de Amora Fini</h6>                    
+                    <p className="card-text">R$ 5,00</p>                    
+                </div>
+            </div>                               
+
+            <div className="card">
+                <img src={amora} class="card-img-top text-center" alt="Balas de Amora Fini" />
+                <div className="card-body">
+                    <h6 className="card-title">Balas de Amora Fini</h6>                    
+                    <p className="card-text">R$ 5,00</p>                    
+                </div>
+            </div>                               
+
+            <div className="card">
+                <img src={amora} class="card-img-top text-center" alt="Balas de Amora Fini" />
+                <div className="card-body">
+                    <h6 className="card-title">Balas de Amora Fini</h6>                    
+                    <p className="card-text">R$ 5,00</p>                    
+                </div>
+            </div>                               
+
+            <div className="card">
+                <img src={amora} class="card-img-top text-center" alt="Balas de Amora Fini" />
+                <div className="card-body">
+                    <h6 className="card-title">Balas de Amora Fini</h6>                    
+                    <p className="card-text">R$ 5,00</p>                    
+                </div>
+            </div>                               
+
+            <div className="card">
+                <img src={amora} class="card-img-top text-center" alt="Balas de Amora Fini" />
+                <div className="card-body">
+                    <h6 className="card-title">Balas de Amora Fini</h6>                    
+                    <p className="card-text">R$ 5,00</p>                    
+                </div>
+            </div>                               
+
+            <div className="card">
+                <img src={amora} class="card-img-top text-center" alt="Balas de Amora Fini" />
+                <div className="card-body">
+                    <h6 className="card-title">Balas de Amora Fini</h6>                    
+                    <p className="card-text">R$ 5,00</p>                    
+                </div>
+            </div>                               
+
+            <div className="card">
+                <img src={amora} class="card-img-top text-center" alt="Balas de Amora Fini" />
+                <div className="card-body">
+                    <h6 className="card-title">Balas de Amora Fini</h6>                    
+                    <p className="card-text">R$ 5,00</p>                    
+                </div>
+            </div>                               
+
+            <div className="card">
+                <img src={amora} class="card-img-top text-center" alt="Balas de Amora Fini" />
+                <div className="card-body">
+                    <h6 className="card-title">Balas de Amora Fini</h6>                    
+                    <p className="card-text">R$ 5,00</p>                    
+                </div>
+            </div>                               
+        </main>
+    )
+}
+
+export default MainPage;
